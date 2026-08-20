33fd9710244aa27c70fe85b21c317d962fa004f1
saitejavandrangi <vandrangisaitejavst@gmail.com>
2026-08-20 17:07:37 +0530

    Improve website.sh with variable usage
    Refactor website.sh to use variables for package management and file handling.

--- website.sh
+++ website.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-sudo apt update
-sudo apt install apache2
+URL=https://www.tooplate.com/zip-templates/2167_orbital.zip
+SRV=apache2
+PKG=apt
+FILE=2167_orbital
+
+sudo $PKG update
+sudo $PKG install $SRV wget unzip -y
+sudo systemctl start $SRV
+sudo systemctl enable $SRV
+sudo wget $URL 
+sudo unzip $FILE
+sudo rm -rf /var/www/html/*
+sudo cp -r $FILE/* /var/www/html/ 
+sudo systemctl restart $SRV
+date
